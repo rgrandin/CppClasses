e3ee985711a0ccfc65ea7c74a77b594bf059ddc1
Robert Grandin <rgrandin@iastate.edu>
2013-03-18 17:37:29 -0500

    Test large VTK files.

--- test/main.cpp
+++ test/main.cpp
@@ -411,7 +411,7 @@ public:
 
 int main()
 {
-    int testcase = 0;   /* 0: Illustrative sample classes defined above. */
+    int testcase = 2;   /* 0: Illustrative sample classes defined above. */
                         /* 1: Run Test() functions for general-use C++ classes. */
                         /* 2: Sandbox area for general testing/debugging. */
 
@@ -546,85 +546,169 @@ int main()
 
 //        delete uv;
 
+        /* Test VTK XML. */
+        if(true){
+//            size_t size1 = 2;
+//            size_t size2 = 2147483647;      /* Max-value of 'int' */
+
+            size_t size1 = 2147483647/200;
+            size_t size2 = 199;
+
+//            while(size1*size2 <= 2147483647){
+//                size2++;
+//            }
+
+            size1 = 16384;
+            size2 = 16384;
+
+            size_t npieces = 9;
+            size_t start_extent = 0;
+            size_t end_extent = size2 - 1;
+
+            Array2D<float> *data = new Array2D<float>(size1, size2*npieces, (float)3);
+
+            std::ofstream file;
+            file.open("TestFile_BINARY.vti", std::ios::binary);
+//            file.open("TestFile_ASCII.vti");
+            char buffer[512];
+            int size;
+
+            if(file.is_open()){
+                size = sprintf(buffer,"<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\">\n");
+                file.write(buffer, size);
+
+                size = sprintf(buffer,"  <ImageData WholeExtent=\"0 %d 0 %d 0 0\" Origin=\"0 0 0\" Spacing=\"1 1 1\">\n", (int)size1-1, (int)size2*(int)npieces-1);
+                file.write(buffer, size);
+
+                for(size_t n=0; n<npieces; n++){
+
+                    size = sprintf(buffer,"    <Piece Extent=\"0 %d %d %d 0 0\">\n", (int)size1-1, (int)start_extent, (int)end_extent);
+                    file.write(buffer, size);
+
+                    start_extent += size2;
+                    end_extent += size2;
+
+                    size = sprintf(buffer,"      <PointData Scalars=\"intensity\">\n");
+                    file.write(buffer, size);
+
+    //                size = sprintf(buffer,"        <DataArray type=\"Int16\" Name=\"array_name\" format=\"appended\" RangeMin=\"3\" RangeMax=\"3\" offset=\"0\" />\n");
+
+                    std::stringstream tmpss;
+                    tmpss << "        <DataArray type=\"Float32\" Name=\"array_name\" format=\"appended\" RangeMin=\"3\" RangeMax=\"3\" offset=\"" << size1*size2*n << "\" />\n";
+                    size = sprintf(buffer,"%s", tmpss.str().c_str());
+                    file.write(buffer, size);
+
+                    size = sprintf(buffer,"      </PointData>\n");
+                    file.write(buffer, size);
+
+                    size = sprintf(buffer,"    </Piece>\n");
+                    file.write(buffer, size);
+
+                }
+
+                size = sprintf(buffer,"  </ImageData>\n");
+                file.write(buffer, size);
+
+                size = sprintf(buffer,"  <AppendedData encoding=\"raw\">\n");
+                file.write(buffer, size);
+
+                size = sprintf(buffer,"   _");
+                file.write(buffer, size);
+
+                file.write((char*)&data->operator [](0), sizeof(float)*size1*size2*npieces);
+
+                size = sprintf(buffer,"\n  </AppendedData>\n");
+                file.write(buffer, size);
+
+                size = sprintf(buffer,"</VTKFile>\n");
+                file.write(buffer, size);
+
+//                file << "<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\">" << std::endl;
+//                file << "  <ImageData WholeExtent=\"0 " << size1-1 << " 0 " << size2-1 << " 0 0\" Origin=\"0 0 0\" Spacing=\"1 1 1\">" << std::endl;
+//                file << "    <Piece Extent=\"0 " << size1-1 << " 0 " << size2-1 << " 0 0\">" << std::endl;
+//                file << "      <PointData Scalars=\"intensity\">" << std::endl;
+//                file << "        <DataArray type=\"Int16\" Name=\"array_name\" format=\"ascii\">" << std::endl;
+//                for(size_t i=0; i<size1; i++){
+//                    for(size_t j=0; j<size2; j++){
+//                        file << data->operator ()(i,j) << " ";
+//                    }
+//                    file << std::endl;
+//                }
+//                file << "        </DataArray>" << std::endl;
+//                file << "      </PointData>" << std::endl;
+//                file << "    </Piece>" << std::endl;
+//                file << "  </ImageData>" << std::endl;
+//                file << "</VTKFile>" << std::endl;
+
+//                file.close();
+
+            } else {
+                std::cerr << "ERROR: Could not open file." << std::endl;
+            }
+
+
+        }
+
+
+
+
+
+        /* Test array-transposition. */
+        if(false){
+
 #ifdef FFTW_TRANSPOSE
-        std::cout << "Transposition tests using FFTW (2D and 3D for 0 <--> 1)" << std::endl;
+            std::cout << "Transposition tests using FFTW (2D and 3D for 0 <--> 1)" << std::endl;
 #else
-        std::cout << "Transposition tests using copy arrays" << std::endl;
+            std::cout << "Transposition tests using copy arrays" << std::endl;
 #endif
 
-        size_t size1 = 2;
-        size_t size2 = 3;
-        size_t size3 = 4;
+            size_t size1 = 2;
+            size_t size2 = 3;
+            size_t size3 = 4;
 
-        if(true){
-            size1 = 450;
-            size2 = 1000;
-            size3 = 360;
-        } else {
-            size1 = 2;
-            size2 = 2;
-            size3 = 2;
-        }
-        Array2D<int> a2d(size1, size2, 0.0f);
-        Array3D<float> a3d(size1, size2, size3, 0.0f);
+            if(true){
+                size1 = 450;
+                size2 = 1000;
+                size3 = 360;
+            } else {
+                size1 = 2;
+                size2 = 2;
+                size3 = 2;
+            }
+            Array2D<int> a2d(size1, size2, 0.0f);
+            Array3D<float> a3d(size1, size2, size3, 0.0f);
 
-        float count = 0.0f;
-        for(size_t k=0; k<size3; k++){
-            for(size_t i=0; i<size1; i++){
-                for(size_t j=0; j<size2; j++){
-                    if(k == 0){
-                        a2d(i,j) = (int)count;
+            float count = 0.0f;
+            for(size_t k=0; k<size3; k++){
+                for(size_t i=0; i<size1; i++){
+                    for(size_t j=0; j<size2; j++){
+                        if(k == 0){
+                            a2d(i,j) = (int)count;
+                        }
+
+                        a3d(i,j,k) = count;
+
+                        count += 1.0f;
                     }
-
-                    a3d(i,j,k) = count;
-
-                    count += 1.0f;
                 }
             }
-        }
 
 
-        Array2D<int> a2d_2(a2d);
-        Array3D<float> a3d_2(a3d);
-        Array3D<float> a3d_original(a3d);
+            Array2D<int> a2d_2(a2d);
+            Array3D<float> a3d_2(a3d);
+            Array3D<float> a3d_original(a3d);
 
-        double t1 = omp_get_wtime();
-        a2d.Transpose();
-        double t2 = omp_get_wtime();
+            double t1 = omp_get_wtime();
+            a2d.Transpose();
+            double t2 = omp_get_wtime();
 
-        float eps = (float)1.0e-5;
-        bool pass = true;
-        for(size_t i=0; i<size1; i++){
-            for(size_t j=0; j<size2; j++){
-
-                float val1 = (float)a2d(j,i);
-                float val2 = (float)a2d_2(i,j);
-                float diff = val2 - val1;
-
-                if(fabs(diff) > eps){
-                    pass = false;
-                }
-
-            }
-        }
-
-        std::cout << std::endl;
-        std::cout << "2D transpose check passed: " << StringManip::BoolToString(pass) << std::endl;
-        std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
-
-
-
-        t1 = omp_get_wtime();
-        a3d.Transpose(1,0);
-        t2 = omp_get_wtime();
-        eps = (float)1.0e-5;
-        pass = true;
-        for(size_t k=0; k<size3; k++){
+            float eps = (float)1.0e-5;
+            bool pass = true;
             for(size_t i=0; i<size1; i++){
                 for(size_t j=0; j<size2; j++){
 
-                    float val1 = a3d(j,i,k);
-                    float val2 = a3d_2(i,j,k);
+                    float val1 = (float)a2d(j,i);
+                    float val2 = (float)a2d_2(i,j);
                     float diff = val2 - val1;
 
                     if(fabs(diff) > eps){
@@ -633,64 +717,91 @@ int main()
 
                 }
             }
-        }
-        a3d = a3d_original;
 
-        std::cout << std::endl;
-        std::cout << "3D transpose (0 <--> 1) check passed: " << StringManip::BoolToString(pass) << std::endl;
-        std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
+            std::cout << std::endl;
+            std::cout << "2D transpose check passed: " << StringManip::BoolToString(pass) << std::endl;
+            std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
 
 
-        t1 = omp_get_wtime();
-        a3d.Transpose(0,2);
-        t2 = omp_get_wtime();
-        pass = true;
-        for(size_t k=0; k<size3; k++){
-            for(size_t i=0; i<size1; i++){
-                for(size_t j=0; j<size2; j++){
 
-                    float val1 = a3d(k,j,i);
-                    float val2 = a3d_2(i,j,k);
-                    float diff = val2 - val1;
+            t1 = omp_get_wtime();
+            a3d.Transpose(1,0);
+            t2 = omp_get_wtime();
+            eps = (float)1.0e-5;
+            pass = true;
+            for(size_t k=0; k<size3; k++){
+                for(size_t i=0; i<size1; i++){
+                    for(size_t j=0; j<size2; j++){
 
-                    if(fabs(diff) > eps){
-                        pass = false;
+                        float val1 = a3d(j,i,k);
+                        float val2 = a3d_2(i,j,k);
+                        float diff = val2 - val1;
+
+                        if(fabs(diff) > eps){
+                            pass = false;
+                        }
+
                     }
-
                 }
             }
-        }
-        a3d = a3d_original;
+            a3d = a3d_original;
 
-        std::cout << std::endl;
-        std::cout << "3D transpose (0 <--> 2) check passed: " << StringManip::BoolToString(pass) << std::endl;
-        std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
+            std::cout << std::endl;
+            std::cout << "3D transpose (0 <--> 1) check passed: " << StringManip::BoolToString(pass) << std::endl;
+            std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
 
-        t1 = omp_get_wtime();
-        a3d.Transpose(1,2);
-        t2 = omp_get_wtime();
-        pass = true;
-        for(size_t k=0; k<size3; k++){
-            for(size_t i=0; i<size1; i++){
-                for(size_t j=0; j<size2; j++){
 
-                    float val1 = a3d(i,k,j);
-                    float val2 = a3d_2(i,j,k);
-                    float diff = val2 - val1;
+            t1 = omp_get_wtime();
+            a3d.Transpose(0,2);
+            t2 = omp_get_wtime();
+            pass = true;
+            for(size_t k=0; k<size3; k++){
+                for(size_t i=0; i<size1; i++){
+                    for(size_t j=0; j<size2; j++){
 
-                    if(fabs(diff) > eps){
-                        pass = false;
+                        float val1 = a3d(k,j,i);
+                        float val2 = a3d_2(i,j,k);
+                        float diff = val2 - val1;
+
+                        if(fabs(diff) > eps){
+                            pass = false;
+                        }
+
                     }
-
                 }
             }
-        }
-        a3d = a3d_original;
+            a3d = a3d_original;
 
-        std::cout << std::endl;
-        std::cout << "3D transpose (1 <--> 2) check passed: " << StringManip::BoolToString(pass) << std::endl;
-        std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
+            std::cout << std::endl;
+            std::cout << "3D transpose (0 <--> 2) check passed: " << StringManip::BoolToString(pass) << std::endl;
+            std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
 
+            t1 = omp_get_wtime();
+            a3d.Transpose(1,2);
+            t2 = omp_get_wtime();
+            pass = true;
+            for(size_t k=0; k<size3; k++){
+                for(size_t i=0; i<size1; i++){
+                    for(size_t j=0; j<size2; j++){
+
+                        float val1 = a3d(i,k,j);
+                        float val2 = a3d_2(i,j,k);
+                        float diff = val2 - val1;
+
+                        if(fabs(diff) > eps){
+                            pass = false;
+                        }
+
+                    }
+                }
+            }
+            a3d = a3d_original;
+
+            std::cout << std::endl;
+            std::cout << "3D transpose (1 <--> 2) check passed: " << StringManip::BoolToString(pass) << std::endl;
+            std::cout << "   Time: " << t2-t1 << " [sec]" << std::endl;
+
+        } /* Control testing of array transposition. */
 
     } /* if(testcase == 2) */
 
