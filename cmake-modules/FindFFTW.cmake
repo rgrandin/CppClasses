d2968a4361a4c944ee0d62f72dbf43727d8e9659
Robert Grandin <rgrandin@iastate.edu>
2013-06-26 09:19:37 -0500

    Create directory for CMake modules used with these classes.

---
+++ cmake-modules/FindFFTW.cmake
@@ -0,0 +1,60 @@
+# 
+# if (FFTW_INCLUDES AND FFTW_LIBRARIES)
+#   set(FFTW_FIND_QUIETLY TRUE)
+# endif (FFTW_INCLUDES AND FFTW_LIBRARIES)
+# 
+# find_path(FFTW_INCLUDES
+#   NAMES
+#   fftw3.h
+#   PATHS
+#   $ENV{FFTWDIR}
+#   ${INCLUDE_INSTALL_DIR}
+# )
+# 
+# find_library(FFTWF_LIB NAMES fftw3f PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
+# find_library(FFTW_LIB NAMES fftw3 PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
+# set(FFTW_LIBRARIES "${FFTWF_LIB} ${FFTW_LIB}" )
+# 
+# find_library(FFTWL_LIB NAMES fftw3l  PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
+# 
+# if(FFTWL_LIB)
+# set(FFTW_LIBRARIES "${FFTW_LIBRARIES} ${FFTWL_LIB}")
+# endif()
+# 
+# 
+# message(STATUS "FFTW ${FFTW_LIBRARIES}" )
+# 
+# include(FindPackageHandleStandardArgs)
+# find_package_handle_standard_args(FFTW DEFAULT_MSG
+#                                   FFTW_INCLUDES FFTW_LIBRARIES)
+# 
+# mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES)
+
+
+
+# From: https://github.com/jedbrown/cmake-modules/blob/master/FindFFTW.cmake
+#       26 June 2013
+#
+#
+# - Find FFTW
+# Find the native FFTW includes and library
+#
+#  FFTW_INCLUDES    - where to find fftw3.h
+#  FFTW_LIBRARIES   - List of libraries when using FFTW.
+#  FFTW_FOUND       - True if FFTW found.
+
+if (FFTW_INCLUDES)
+  # Already in cache, be silent
+  set (FFTW_FIND_QUIETLY TRUE)
+endif (FFTW_INCLUDES)
+
+find_path (FFTW_INCLUDES fftw3.h)
+
+find_library (FFTW_LIBRARIES NAMES fftw3)
+
+# handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
+
+mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
